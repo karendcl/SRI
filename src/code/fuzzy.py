2547a0bd5c5336ce0ea99c003f12b933d342ef3a
karendcl <karen.canterolopez@gmail.com>
2024-02-26 22:01:45 -0500

    changing from MMM to Paice

--- src/code/fuzzy.py
+++ src/code/fuzzy.py
@@ -22,24 +22,11 @@ def indices_of_words_from_query(query, vectorizer):
     return [vectorizer.vocabulary_[word] for word in query if word in vectorizer.vocabulary_]
 
 
-def Similitud_MMM(query_ind, matrix_tfidf, doc_ind: int):
-
-    co1 = 0.2
-    co2 = 0.7
-
-    cy1 = 0.2
-    cy2 = 0.7
-
-    min = inf
-    max = -inf
-
+def Similitud_Paice(query_ind, matrix_tfidf, doc_ind: int):
+    sum = 0
     for i in query_ind:
-        if matrix_tfidf[doc_ind, i] < min:
-            min = matrix_tfidf[doc_ind, i]
-        if matrix_tfidf[doc_ind, i] > max:
-            max = matrix_tfidf[doc_ind, i]
-
-    return co1 * min + co2 * max, cy1 * min + cy2 * max
+        sum += matrix_tfidf[doc_ind, i]
+    return sum
 
 
 
@@ -51,12 +38,10 @@ def FuzzyModel(query, documents, metrics):
     query = [word for word in query if word in vectorizer.vocabulary_]
     indices = indices_of_words_from_query(query, vectorizer)
 
-    scores = [Similitud_MMM(indices, matrix_tfidf, i) for i in range(len(documents))]
-
-    mean_score_doc = [sum(score) / len(score) for score in scores]
+    scores = [Similitud_Paice(indices, matrix_tfidf, i) for i in range(len(documents))]
 
     #order the documents by the mean score and return the index of the documents
-    ordered =  sorted(range(len(mean_score_doc)), key=lambda k: mean_score_doc[k], reverse=True)
+    ordered = sorted(range(len(scores)), key=lambda k: scores[k], reverse=True)
 
     #return the indices of docs that have a score greater than 0
-    return [i for i in ordered if mean_score_doc[i] > 0.1]
+    return [i for i in ordered if scores[i] > 0.2]
