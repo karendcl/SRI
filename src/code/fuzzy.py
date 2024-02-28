6e1c5bb7e678228a61a05750d38e66f2b686e87a
Dráckaro <luisalejandrorodriguezotero@gmail.com>
2024-02-28 02:03:14 -0500

    Refactorized code an reduced preprocessing time

--- src/code/fuzzy.py
+++ src/code/fuzzy.py
@@ -1,19 +1,9 @@
 from sklearn.feature_extraction.text import TfidfVectorizer
-import pickle
-from math import inf
 
-def Matrix_TFIDF(metrics):
-    if metrics:
-        pkl_vect_name = 'vect_for_metrics.pkl'
-        pkl_matrix_name = 'matrix_tfidf_for_metrics.pkl'
-    else:
-        pkl_vect_name = 'vect.pkl'
-        pkl_matrix_name = 'matrix_tfidf.pkl'
-
-    with open(pkl_vect_name, 'rb') as f:
-        vectorizer = pickle.load(f)
-    with open(pkl_matrix_name, 'rb') as f:
-        matrix_tfidf = pickle.load(f)
+def Matrix_TFIDF(documents):
+    docs = [' '.join(doc) for doc in documents]
+    vectorizer = TfidfVectorizer()
+    matrix_tfidf = vectorizer.fit_transform(docs)
 
     return vectorizer, matrix_tfidf
 
@@ -22,26 +12,25 @@ def indices_of_words_from_query(query, vectorizer):
     return [vectorizer.vocabulary_[word] for word in query if word in vectorizer.vocabulary_]
 
 
-def Similitud_Paice(query_ind, matrix_tfidf, doc_ind: int):
+def Paice_Similarity(query_ind, matrix_tfidf, doc_ind: int):
     sum = 0
     for i in query_ind:
         sum += matrix_tfidf[doc_ind, i]
     return sum
 
 
+def FuzzyModel(query, documents):
 
-def FuzzyModel(query, documents, metrics):
-
-    vectorizer, matrix_tfidf = Matrix_TFIDF(metrics)
+    vectorizer, matrix_tfidf = Matrix_TFIDF(documents)
 
     query = query.split()
     query = [word for word in query if word in vectorizer.vocabulary_]
     indices = indices_of_words_from_query(query, vectorizer)
 
-    scores = [Similitud_Paice(indices, matrix_tfidf, i) for i in range(len(documents))]
+    scores = [Paice_Similarity(indices, matrix_tfidf, i) for i in range(len(documents))]
 
     #order the documents by the mean score and return the index of the documents
     ordered = sorted(range(len(scores)), key=lambda k: scores[k], reverse=True)
 
     #return the indices of docs that have a score greater than 0
-    return [i for i in ordered if scores[i] > 0.2]
+    return [i for i in ordered if scores[i] > 0]
--- src/code/process_docs.py
+++ src/code/process_docs.py
@@ -3,53 +3,26 @@ import spacy
 import gensim
 
 def tokenization_spacy(texts):
-        nlp = spacy.load("en_core_web_sm")
-        return [[token for token in nlp(doc)] for doc in texts]
+    nlp = spacy.load("en_core_web_sm", enable=["lemmatizer"])
+    return [[token for token in nlp(doc)] for doc in texts]
 
 
 def remove_noise_spacy(tokenized_docs):
-        return [[token for token in doc if token.is_alpha] for doc in tokenized_docs]
+    return [[token for token in doc if token.is_alpha] for doc in tokenized_docs]
 
 
 def remove_stopwords_spacy(tokenized_docs):
-        stopwords = spacy.lang.en.stop_words.STOP_WORDS
-        return [
-        [token for token in doc if token.text not in stopwords] for doc in tokenized_docs
-        ]
+    stopwords = spacy.lang.en.stop_words.STOP_WORDS
+    return [
+    [token for token in doc if token.text not in stopwords] for doc in tokenized_docs]
 
 
 def morphological_reduction_spacy(tokenized_docs, use_lemmatization=True):
-        stemmer = nltk.stem.PorterStemmer()
-        return [
-        [token.lemma_ if use_lemmatization else stemmer.stem(token.text) for token in doc]
-        for doc in tokenized_docs
-        ]
+    stemmer = nltk.stem.PorterStemmer()
+    return [
+    [token.lemma_ if use_lemmatization else stemmer.stem(token.text) for token in doc]
+    for doc in tokenized_docs]
 
-
-def filter_tokens_by_occurrence(tokenized_docs, no_below=5, no_above=0.5):
-        
-        dictionary = gensim.corpora.Dictionary(tokenized_docs)
-        dictionary.filter_extremes(no_below=no_below, no_above=no_above)
-
-        filtered_words = [word for _, word in dictionary.iteritems()]
-        filtered_tokens = [
-        [word for word in doc if word in filtered_words]
-        for doc in tokenized_docs
-        ]
-
-        return filtered_tokens
-
-
-def vector_representation(tokenized_docs, dictionary, vector_repr, use_bow=True):
-    corpus = [dictionary.doc2bow(doc) for doc in tokenized_docs]
-
-    if use_bow:
-        vector_repr = corpus
-    else:
-        tfidf = gensim.models.TfidfModel(corpus)
-        vector_repr = [tfidf[doc] for doc in corpus]
-
-    return vector_repr
 
 def processed_docs(Corpus):
     #Load corpus
@@ -60,23 +33,18 @@ def processed_docs(Corpus):
         documents.append(CurrentDoc)
 
     tokenized_docs = []
-    vector_repr = []
     dictionary = {}
-    vocabulary = []
 
     tokenized_docs = morphological_reduction_spacy(remove_stopwords_spacy(remove_noise_spacy(tokenization_spacy(documents))), True)
-    tokenized_docs = filter_tokens_by_occurrence(tokenized_docs)
     dictionary = gensim.corpora.Dictionary(tokenized_docs)
-    vector_repr = vector_representation(tokenized_docs, dictionary, vector_repr)
-    vocabulary = list(dictionary.token2id.keys())
 
-    return tokenized_docs, vector_repr, dictionary, vocabulary
+    return tokenized_docs, dictionary
 
 def processed_query(query):
-      query = query.lower()
-      Query = []
-      Query.append(query)
+    query = query.lower()
+    Query = []
+    Query.append(query)
 
-      processed_Query = morphological_reduction_spacy(remove_stopwords_spacy(remove_noise_spacy(tokenization_spacy(Query))), True)
+    processed_Query = morphological_reduction_spacy(remove_stopwords_spacy(remove_noise_spacy(tokenization_spacy(Query))), True)
 
-      return ' '.join(processed_Query[0])
+    return ' '.join(processed_Query[0])
